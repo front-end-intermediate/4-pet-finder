3bd31442268288fb3a6dcea204ac46aedd40eaa1
DannyBoyNYC <daniel.deverell@gmail.com>
2024-04-07 11:03:55 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,48 +1,17 @@
-import { useEffect, useState } from "react";
-import { createRoot } from "react-dom/client";
-import { Pet } from "./Pet";
+import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
 
 const App = () => {
-  const [pets, setPets] = useState([]);
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    async function getData() {
-      setLoading(true);
-      try {
-        const res = await fetch("http://localhost:3001/pets");
-        const pets = await res.json();
-        setPets(pets);
-        setLoading(false);
-      } catch (e) {
-        setLoading(false);
-      }
-    }
-    getData();
-  }, []);
-
   return (
     <main>
       <h1>Adopt-a-Pet</h1>
-      {isLoading ? (
-        <div className="loading">Loading...</div>
-      ) : (
-        <>
-          <ul>
-            {pets.map((pet) => (
-              <li key={pet.id}>
-                <Pet pet={pet} />
-              </li>
-            ))}
-          </ul>
-          <button>Add a Pet</button>
-        </>
-      )}
+      <ul>
+        <li>pets go here</li>
+      </ul>
+      <button>Add a Pet</button>
     </main>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM.render(<App />, document.querySelector("#root"));
